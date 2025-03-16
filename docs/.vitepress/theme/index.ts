1a5c9e9159b8128f9b3d2904928f8c02ec4c7a89
euuen <3048721251@qq.com>
2025-03-16 15:08:49 +0800

    fixed bug

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
+import { h, watch} from 'vue'
+import { Theme, useRoute} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import smiles from './smiles.vue'
@@ -16,5 +16,18 @@ export default {
     // ...
     // app.component('SmilesDrawer', SmilesDrawer);
     app.component("smiles", smiles);
+  },
+  setup() {
+	const route = useRoute();
+	watch(
+		()=> route.path,
+		()=>{
+			if(typeof window != 'undefined'){//这里是防止你publish到github上可能会发生的报错
+				// alert("jjs");
+				// MathJax.typesetPromise();//让mathjax重新渲染
+        setTimeout("MathJax.typesetPromise()", 200);
+			}
+		}
+	);
   }
 } satisfies Theme
