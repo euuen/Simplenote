3476fdaf101f6e3b992b0514d6a7e117e3b5595b
euuen <3048721251@qq.com>
2025-03-22 10:21:03 +0800

    add comment system

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import { h, watch} from 'vue'
-import { Theme, useRoute} from 'vitepress'
+import { h, watch, toRefs} from 'vue'
+import { Theme, useRoute, useData} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
 import './style.css'
 import smiles from './smiles.vue'
+import theme from 'vitepress/theme'
 
 export default {
   extends: DefaultTheme,
@@ -13,21 +15,33 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
-    // app.component('SmilesDrawer', SmilesDrawer);
     app.component("smiles", smiles);
   },
+
   setup() {
-	const route = useRoute();
-	watch(
-		()=> route.path,
-		()=>{
-			if(typeof window != 'undefined'){//这里是防止你publish到github上可能会发生的报错
-				// alert("jjs");
-				// MathJax.typesetPromise();//让mathjax重新渲染
-        setTimeout("MathJax.typesetPromise()", 200);
-			}
-		}
-	);
+    const {frontmatter} = useData();
+    const route = useRoute();
+    watch(
+      ()=> route.path,
+      ()=>{
+        if(typeof window != 'undefined'){//这里是防止你publish到github上可能会发生的报错
+          // MathJax.typesetPromise();//让mathjax重新渲染
+          setTimeout("MathJax.typesetPromise()", 200);
+        }
+      }
+    );
+    giscusTalk({
+      repo: 'euuen/Simplenote', 
+      repoId: 'R_kgDON3qnFA',   
+      category: 'Announcements', // default: `General` 
+      categoryId: 'DIC_kwDON3qnFM4CoNve', 
+      mapping: 'pathname', // default: `pathname`
+      inputPosition: 'top', // default: `top`
+      lang: 'zh-CN', // default: `zh-CN`
+      lightTheme: 'light', // default: `light`
+      darkTheme: 'transparent_dark', 
+    },{
+      frontmatter, route
+    }, true);
   }
 } satisfies Theme
